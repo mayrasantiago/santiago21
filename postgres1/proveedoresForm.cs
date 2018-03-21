19ba2ce926817d6cc4bc70be8205e71703f4221c
mayrasantiago <princesa-love96@hotmail.com>
2018-03-20 19:28:11 -0600

    se agrga formulario a tabla proveedores

---
+++ postgres1/proveedoresForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace postgres1
+{
+    public partial class proveedoresForm : Form
+    {
+        public proveedoresForm()
+        {
+            InitializeComponent();
+        }
+
+        private void proveedoresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.proveedoresBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.basedatos1DataSet);
+
+        }
+
+        private void proveedoresForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'basedatos1DataSet.proveedores' Puede moverla o quitarla según sea necesario.
+            this.proveedoresTableAdapter.Fill(this.basedatos1DataSet.proveedores);
+
+        }
+    }
+}
